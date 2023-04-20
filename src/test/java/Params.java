ac334cae6e2e67cd3f97c0d97a3a7b6888bc88ae
“mehmetkucukkoc” <mhmtkckkc@gmail.com>
2023-04-20 16:32:57 +0300

    testngBasics2

--- src/test/java/Params.java
+++ src/test/java/Params.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,9 +17,8 @@ public class Params {
         System.out.println("The final difference of the given values is " + finaldiff);
     }
     @Test
-    @Parameters ({"val1", "val2"})
-    public void mul(int v1, int v2) {
-        int prod = v1*v2;
-        System.out.println("The Product Of Value 1 and 2 is " + prod);
+    @Parameters ("message")
+    public void OP( @Optional("Optional Parameter Selected") String message) {
+        System.out.println(message);
     }
 }
